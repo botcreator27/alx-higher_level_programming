616664c9f3dcf5242a47739343568f25ee491b5e
Bot Creator <mawutorcundo@gmail.com>
2024-09-11 09:06:46 -0700

    fixed last digit for neg number to have neg sign

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,11 +1,15 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-last_digit = int(str(number)[-1])
-if last_digit > 5:
+if number > 0:
+    last_digit = str(number)[-1]
+else:
+    last_digit = '-' + str(number)[-1]
+
+if int(last_digit) > 5:
     string = "and is greater than 5"
-elif last_digit == 0:
+elif int(last_digit) == 0:
     string = "and is 0"
-elif last_digit < 6 and last_digit != 0:
+elif int(last_digit) < 6 and last_digit != 0:
     string = "and is less than 6 and not 0"
 print(f"Last digit of {number} is {last_digit} {string}")
