e512541963f8ab9d47fbd2cfa3e4989039fb63d9
Bot Creator <mawutorcundo@gmail.com>
2024-09-18 11:40:58 -0700

    this replaces element with specified replacement in new list

---
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+def search_replace(my_list, search, replace):
+    new_list = []
+    for element in my_list:
+        new_list.append(element)
+        if element == search:
+            new_list.append(replace)
+    return new_list
