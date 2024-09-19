7d158db1df442a7d286cd8be2643333122d7cc65
Bot Creator <mawutorcundo@gmail.com>
2024-09-19 08:58:30 -0700

    update the dictionary by replacing or creating new kv pair

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
 
 def print_sorted_dictionary(a_dictionary):
-    for x in sorted(a_dictionary):
-        print(f"{x}: {a_dictionary[x]}")
+    if a_dictionary is None:
+        return
+    else:
+        for x in sorted(a_dictionary):
+            print(f"{x}: {a_dictionary[x]}")
