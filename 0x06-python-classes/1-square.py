fea21bbff3fbe752abd3c29c4b953161f552fed6
Bot Creator <mawutorcundo@gmail.com>
2024-10-01 12:24:32 -0700

    initialise class square with private attribute

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+class Square:
+    def __init__(self, size):
+        """Initialising class Square with private attribute
+           Args:
+           size: holds the sixe of square
+        """
+        self.__size = size
