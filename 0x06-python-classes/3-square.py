6b4358db62735ddaf70fd621c14e59ab97e632ae
Bot Creator <mawutorcundo@gmail.com>
2024-10-01 15:03:48 -0700

    returns the square area of an instance of class square

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+""" Define the Class Square"""
+
+
+class Square:
+    """
+    Defines properties of square (based on 0-square.py).
+
+    Attributes:
+        __size(int): size of square.
+    """
+    def __init__(self, size=0):
+        """
+        Initialises the class square (1 side).
+
+        Args:
+            size: size of square.
+        """
+        self.__size = size
+
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
+
+    def area(self):
+        """calculates the area of the square
+
+            Returns:the sq area
+        """
+        return self.__size ** 2
