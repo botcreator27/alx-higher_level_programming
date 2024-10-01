5f377484e8304aba0883827d67a4c4d531406064
Bot Creator <mawutorcundo@gmail.com>
2024-10-01 12:32:43 -0700

    fixed documentation

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,9 +1,16 @@
 #!/usr/bin/python3
+""" Create a class Square
+
+Attributes:
+    __size: the size of the square.
+"""
+
 
 class Square:
     def __init__(self, size):
         """Initialising class Square with private attribute
-           Args:
-           size: holds the sixe of square
+
+        Args:
+           size: holds the size of square
         """
         self.__size = size
