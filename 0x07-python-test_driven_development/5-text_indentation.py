baad8e946555689aac8c6618c64e1ca24afbdd1d
Bot Creator <mawutorcundo@gmail.com>
2024-10-07 08:11:23 -0700

    fixed beginning whitespace problem

---
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+""" this module defines a function that organizes text on new lines """
+
+
+def text_indentation(text):
+    """
+    this function indents the text after identifying '.','?' and ':'
+    characters
+
+    Args:
+        Text(str): the text to be indented
+
+    Raises:
+        TypeError: text must be a string
+    """
+    if type(text) is not str:
+        raise TypeError("text must be a string")
+
+    result = []
+
+    for char in text:
+        if char in ".?:":
+            result.append(char)
+            result.append("\n\n")
+        elif char == " " and (len(result) > 0 and result[-1] == "\n\n"):
+            continue
+        else:
+            result.append(char)
+
+    text = ''.join(result)
+    print("{}".format(text))
