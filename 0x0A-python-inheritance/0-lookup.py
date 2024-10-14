155cf88b34f0e987ffe0315ac95a777c88b6305b
Bot Creator <mawutorcundo@gmail.com>
2024-10-14 14:36:45 -0700

    task 1 print out attributes of an obj check

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+""" this module describes the lookup function """
+
+
+def lookup(obj):
+    """ this function return a list of all attributes of the class """
+    return dir(obj)
---
+++ 0x0A-python-inheritance/README.md
@@ -0,0 +1,1 @@
+welcome, lets explore python inheritance today
