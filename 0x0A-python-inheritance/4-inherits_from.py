514bc0e74347a7787e8f63f5277fa92bc3a87778
Bot Creator <mawutorcundo@gmail.com>
2024-10-15 07:00:29 -0700

    pycodestyle check!

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -10,4 +10,4 @@ def inherits_from(obj, a_class):
     Returns:
         true - if is instance of subclass but not a_class, fail- if not
     """
-    return issubclass(type(obj), a_class) and type(obj) != a_class
+    return issubclass(type(obj), a_class) and type(obj) is not a_class
