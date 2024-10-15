f594cdd271c7d5170b86329e2a852c7c39457132
Bot Creator <mawutorcundo@gmail.com>
2024-10-15 08:23:08 -0700

    done

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+""" class Base """
+
+
+class BaseGeometry():
+    """ class BaseGeometry with public instance method"""
+    def area(self):
+        """ function raises an exception """
+
+        raise Exception("area() is not implemented")
