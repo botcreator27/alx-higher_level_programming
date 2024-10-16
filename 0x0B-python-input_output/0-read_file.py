0b45f7c62838bac7c76954c0ae2273b674636a05
Bot Creator <mawutorcundo@gmail.com>
2024-10-16 11:57:23 -0700

    done

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -4,5 +4,5 @@
 
 def read_file(filename=""):
     """  reads a text file (UTF8) and prints it to stdout """
-    with open(filename, "r", encoding="utf-8") as a_file:
-        print(a_file.read())
+    with open(filename, "r", encoding="utf-8") as f:
+        print(f.read(), end="")
