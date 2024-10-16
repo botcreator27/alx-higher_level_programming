ccfada7d4ceb244dcd4a1c4da256330a77559983
Bot Creator <mawutorcundo@gmail.com>
2024-10-16 03:45:44 -0700

    class Square check

---
+++ 0x0A-python-inheritance/11-square.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+""" this module defines class Square, Rectangle subclass"""
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """ class square """
+    def __init__(self, size):
+        """ initialises the private attr size
+        Args:
+            size: must be a positive int
+        """
+        self.integer_validator("size", size)
+        self.__size = size
+        super().__init__(size, size)
+
+    def area(self):
+        """ revises how area computes """
+        return self.__size ** 2
+
+    def __str__(self):
+        """ prints the square description """
+        return "[Square] {:d}/{:d}".format(self.__size, self.__size)
