c5d9f523632c665853fad4364d9ce0ce4becdd3b
Bot Creator <mawutorcundo@gmail.com>
2024-10-17 06:59:40 -0700

    done

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+""" this module describes a write function """
+
+
+def write_file(filename="", text=""):
+    """ writes to a file(overwrites if already existing)
+    Args:
+        filename: filename
+        text(str): text to be written
+    """
+    print(len(text))
+    with open(filename, "w", encoding="utf-8") as f:
+        return f.write(text)
+
