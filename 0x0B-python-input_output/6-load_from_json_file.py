0a220def87e447b10b009fe8e158930eff229abe
Bot Creator <mawutorcundo@gmail.com>
2024-10-17 11:29:06 -0700

    done

--- 0x0B-python-input_output/6-load_from_json_file.py
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -8,5 +8,5 @@ def load_from_json_file(filename):
     Args:
         filename: the json file
     """
-    with open(filename, "w", encoding="utf-8") as f:
+    with open(filename, "r", encoding="utf-8") as f:
         return json.loads(f)
