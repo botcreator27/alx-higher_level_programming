77ce031716072c6c162b6e81b6f85f6f002c4d1f
Bot Creator <mawutorcundo@gmail.com>
2024-10-17 08:44:50 -0700

    done

--- 0x0B-python-input_output/3-to_json_string.py
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
-import json
 """ module converts to json format """
+import json
 
 
 def to_json_string(my_obj):
     """ funicction returns the json format of my_obj
     Args:
-        my_obj: python object 
+        my_obj: python object
     """
     return json.dumps(my_obj)
