275b61bef2adf26ca3ea9c8ffc300e64239dd149
Bot Creator <mawutorcundo@gmail.com>
2024-10-17 11:11:34 -0700

    create obj from json file

---
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""module create an obj rom json file"""
+import json
+
+
+def load_from_json_file(filename):
+    """ function converts json file to obj
+    Args:
+        filename: the json file
+    """
+    with open(filename, "w", encoding="utf-8") as f:
+        return json.loads(f)
