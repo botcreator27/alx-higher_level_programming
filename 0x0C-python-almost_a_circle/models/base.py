004c35a22f56b0e346f61a6f4e41004bbd56e6ab
botcreator27 <mawutorcundo@gmail.com>
2024-10-23 12:42:58 -0700

    dict to json format

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 """ this module defines a class base """
+import json
 
 
 class Base:
@@ -17,3 +18,10 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """returns the json format of list_dictionaries"""
+        if list_dictionaries is None or not list_dictionaries:
+            return []
+        return json.dumps(list_dictionaries)
