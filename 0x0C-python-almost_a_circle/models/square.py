4646d72c0c0118a763b55c8d35652087d9d72bce
botcreator27 <mawutorcundo@gmail.com>
2024-10-23 12:20:23 -0700

    to dict

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -48,3 +48,8 @@ class Square(Rectangle):
             self.__update(*args)
         elif kwargs:
             self.__update(**kwargs)
+
+    def to_dictionary(self):
+        """represents Square as a dict"""
+        return {"id": self.id, "size": self.size, "x": self.x,
+                "y": self.y}
