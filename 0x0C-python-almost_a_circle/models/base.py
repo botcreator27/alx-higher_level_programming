f9b4de61c5f5f6088415a270ceb9294801f4b866
botcreator27 <mawutorcundo@gmail.com>
2024-10-23 12:47:56 -0700

    requirement check

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -23,5 +23,5 @@ class Base:
     def to_json_string(list_dictionaries):
         """returns the json format of list_dictionaries"""
         if list_dictionaries is None or not list_dictionaries:
-            return []
+            return "[]"
         return json.dumps(list_dictionaries)
