c100fcfbbb7e48e8fe5f4a426da2053c11c2c19a
botcreator27 <mawutorcundo@gmail.com>
2024-10-23 06:06:41 -0700

    display method for rectangle

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -78,8 +78,8 @@ class Rectangle(Base):
 
     def __str__(self):
         """prints out rectangle details"""
-        print("[Rectangle] ({}) {}/{} - {width}/{height}".format(
-                self.__id, self.__x, self.__y, self.__width, self.__height))
+        return ("[Rectangle] ({}) {}/{} - {}/{}".format(
+                self.id, self.__x, self.__y, self.__width, self.__height))
 
     def area(self):
         """ public mthod to compute area"""
