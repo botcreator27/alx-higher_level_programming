224a91c0ec606f0ccf1923604bdacd18d74e417c
botcreator27 <mawutorcundo@gmail.com>
2024-10-23 10:30:32 -0700

    class square

---
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+from models.rectangle import Rectangle
+""" this module defines class square- a rectangle subclass"""
+
+
+class Square(Rectangle):
+    """class square"""
+    def __init__(self, size, x=0, y=0, id=None):
+        """initialises attributes"""
+        super().__init__(size, size, x, y, id)
+
+    def __str__(self):
+        """displays a Square details"""
+        return "[Square] (<{}>) <{}>/<{}> - size".format(
+                self.id, self.x, self.y, self.width)
