e4a1f44faaa8d1c5b6d07a8ca47870712e6fa24a
botcreator27 <mawutorcundo@gmail.com>
2024-10-23 11:50:11 -0700

    rect to dictionary

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -117,5 +117,6 @@ class Rectangle(Base):
 
     def to_dictionary(self):
         """represents the class as a dict"""
-        return {"id": self.id, "size": self.width,
-                "x": self.x, "y": self.y}
+        return {"id": self.id, "width": self.__width,
+                "height": self.__height
+                "x": self.__x, "y": self.__y}
